10a2283a756505498d3c3f303c7830c3bb5a43b9
Vanessa Ortega <ortegavan@live.com>
2024-03-17 14:20:36 -0300

    feat: criadas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { JobsComponent } from './routes/jobs/jobs.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'jobs', pathMatch: 'full' },
+    {
+        path: 'jobs',
+        component: JobsComponent,
+        children: [
+            {
+                path: ':id',
+                loadComponent: () =>
+                    import('./routes/job/job.component').then(
+                        (m) => m.JobComponent
+                    ),
+            },
+        ],
+    },
+    { path: '**', redirectTo: 'jobs' },
+];
